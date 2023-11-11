3277ddba41e94b28a9a778b83b8651d122c9b346
Azfa <149128367+Azfa34@users.noreply.github.com>
2023-11-12 00:29:22 +0800

    RESET PASSWORD FUNCTION (COMPLETED)

--- app/src/main/java/com/example/ellymartarkedcengal/ForgotPassword.java
+++ app/src/main/java/com/example/ellymartarkedcengal/ForgotPassword.java
@@ -1,18 +1,27 @@
 package com.example.ellymartarkedcengal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ForgotPassword extends AppCompatActivity {
     EditText adminEmailEditText;
     Button resetPasswordButton;
     Button loginButton;
+    FirebaseAuth mAuth;
+    String strEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,31 +30,39 @@ public class ForgotPassword extends AppCompatActivity {
         adminEmailEditText = findViewById(R.id.etAdminEmail);
         resetPasswordButton = findViewById(R.id.btnAdminReset);
         loginButton = findViewById(R.id.btnAdminLogin);
+        mAuth = FirebaseAuth.getInstance();
 
         resetPasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = adminEmailEditText.getText().toString();
-                if (isValidEmail(email)){
-                    Toast.makeText(ForgotPassword.this, "Password reset email sent to " + email, Toast.LENGTH_SHORT).show();
+                strEmail = adminEmailEditText.getText().toString().trim();
+                if (!TextUtils.isEmpty(strEmail)) {
+                    ResetPassword();
+                } else {
+                    adminEmailEditText.setError("Invalid email address");
                 }
-                else {
-                    Toast.makeText(ForgotPassword.this, "Invalid email address", Toast.LENGTH_SHORT).show();
-                }
-
-                loginButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(getApplicationContext(),AdminLogin.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                });
             }
         });
     }
 
-    private boolean isValidEmail(CharSequence target) {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+
+        private void ResetPassword() {
+            resetPasswordButton.setVisibility(View.VISIBLE);
+            mAuth.sendPasswordResetEmail(strEmail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Toast.makeText(ForgotPassword.this, "Password reset email sent to " + adminEmailEditText, Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(ForgotPassword.this,AdminLogin.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(ForgotPassword.this, "Error : -" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    resetPasswordButton.setVisibility(View.VISIBLE);
+                }
+            });
+        }
     }
-}
+
