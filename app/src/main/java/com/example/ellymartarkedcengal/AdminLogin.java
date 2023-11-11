66de6e4136900de87749c22419fc659e51b481f8
Azfa <149128367+Azfa34@users.noreply.github.com>
2023-11-11 12:46:16 +0800

    BACKEND (FIREBASE AUTH)

--- app/src/main/java/com/example/ellymartarkedcengal/AdminLogin.java
+++ app/src/main/java/com/example/ellymartarkedcengal/AdminLogin.java
@@ -1,7 +1,9 @@
 package com.example.ellymartarkedcengal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.View;
@@ -9,16 +11,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AdminLogin extends AppCompatActivity {
 
     private EditText adminEmailEditText, adminPasswordEditText;
     private Button loginButton,forgotPassButton;
+    FirebaseAuth mAuth;
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
-
+        mAuth = FirebaseAuth.getInstance();
         adminEmailEditText = findViewById(R.id.etAdminEmail);
         adminPasswordEditText = findViewById(R.id.etAdminPassword);
         loginButton = findViewById(R.id.btnAdminLogin);
@@ -30,11 +49,32 @@ public class AdminLogin extends AppCompatActivity {
                 // Retrieve admin login data from the EditText fields
                 String adminEmail = adminEmailEditText.getText().toString();
                 String adminPassword = adminPasswordEditText.getText().toString();
-
+                Intent intent = new Intent(getApplicationContext(), AdminRegistration.class);
+                startActivity(intent);
+                finish();
                 // You can implement your login logic here, e.g., check if the provided credentials are valid.
 
                 // For this basic example, show a simple message to indicate a successful login.
                 Toast.makeText(AdminLogin.this, "Admin login successful!", Toast.LENGTH_SHORT).show();
+
+                mAuth.signInWithEmailAndPassword(adminEmail, adminPassword)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(getApplicationContext(),"Login Successful", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(AdminLogin.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
             }
         });
     }
--- app/src/main/java/com/example/ellymartarkedcengal/AdminRegistration.java
+++ app/src/main/java/com/example/ellymartarkedcengal/AdminRegistration.java
@@ -1,24 +1,46 @@
 package com.example.ellymartarkedcengal;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AdminRegistration extends AppCompatActivity {
 
     private EditText adminNameEditText, adminEmailEditText, adminPasswordEditText;
     private Button registerButton;
+    FirebaseAuth mAuth;
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_registration);
-
+        mAuth = FirebaseAuth.getInstance();
         adminNameEditText = findViewById(R.id.etAdminName);
         adminEmailEditText = findViewById(R.id.etAdminEmail);
         adminPasswordEditText = findViewById(R.id.etAdminPassword);
@@ -38,6 +60,23 @@ public class AdminRegistration extends AppCompatActivity {
                 Toast.makeText(AdminRegistration.this, "Admin registered successfully!", Toast.LENGTH_SHORT).show();
 
                 startActivity(new Intent(AdminRegistration.this, AdminLogin.class));
+
+                mAuth.createUserWithEmailAndPassword(adminEmail, adminPassword)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(AdminRegistration.this, "Authentication created.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(AdminRegistration.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
             }
         });
     }
