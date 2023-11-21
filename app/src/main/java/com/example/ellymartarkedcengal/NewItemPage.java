bfbfe9212315f9ccb9ed2054dd1942dfa3160cfd
NurulNajihah-web <149048762+NurulNajihah-web@users.noreply.github.com>
2023-11-21 10:14:19 +0800

    NewItemPage

---
+++ app/src/main/java/com/example/ellymartarkedcengal/NewItemPage.java
@@ -0,0 +1,14 @@
+package com.example.ellymartarkedcengal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewItemPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_item_page);
+    }
+}
