0b9e44adbd43313b22694135604a750a56cec628
NurulNajihah-web <149048762+NurulNajihah-web@users.noreply.github.com>
2023-11-26 23:59:07 +0800

    Navigation Layout

--- app/src/main/java/com/example/ellymartarkedcengal/Dashboard.java
+++ app/src/main/java/com/example/ellymartarkedcengal/Dashboard.java
@@ -1,14 +1,67 @@
 package com.example.ellymartarkedcengal;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Dashboard extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.home) {
+                    Toast.makeText(Dashboard.this, "Home Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.wishlist) {
+                    Toast.makeText(Dashboard.this, "Wishlist Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.activity) {
+                    Toast.makeText(Dashboard.this, "Activity Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.profile) {
+                    Toast.makeText(Dashboard.this, "Profile Selected", Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
