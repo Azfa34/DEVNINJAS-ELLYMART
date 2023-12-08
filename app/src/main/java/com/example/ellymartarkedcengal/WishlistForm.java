f41866e2f487c52dfc4bff530e984414b4e8fac0
NurulNajihah-web <149048762+NurulNajihah-web@users.noreply.github.com>
2023-12-08 18:28:24 +0800

    WishList Form

--- app/src/main/java/com/example/ellymartarkedcengal/WishlistForm.java
+++ app/src/main/java/com/example/ellymartarkedcengal/WishlistForm.java
@@ -1,14 +1,96 @@
 package com.example.ellymartarkedcengal;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class WishlistForm extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private ImageView imageView;
+    private Uri selectedImageUri;
+
+    // Firebase Storage
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wishlist_form);
+
+        imageView = findViewById(R.id.imageViewItem1);
+        Button btnPickPhotoWish = findViewById(R.id.btnPickPhotoWish);
+
+        // Initialize Firebase Storage
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        btnPickPhotoWish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImagePicker();
+            }
+        });
+    }
+
+    private void openImagePicker() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_PICK);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            selectedImageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImageToFirebase() {
+        if (selectedImageUri != null) {
+            StorageReference imageRef = storageReference.child("images/" + System.currentTimeMillis() + ".jpg");
+
+            imageView.setDrawingCacheEnabled(true);
+            imageView.buildDrawingCache();
+            Bitmap bitmap = imageView.getDrawingCache();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] data = baos.toByteArray();
+
+            // Upload image to Firebase Storage
+            UploadTask uploadTask = imageRef.putBytes(data);
+
+            uploadTask.addOnSuccessListener(taskSnapshot -> {
+
+                imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                    String downloadUrl = uri.toString();
+
+                });
+            }).addOnFailureListener(e -> {
+                e.printStackTrace();
+            });
+        }
     }
 }
