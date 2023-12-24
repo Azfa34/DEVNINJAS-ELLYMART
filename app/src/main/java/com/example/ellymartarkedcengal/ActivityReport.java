d9e7b53dc90dd95101a908f73cb43afc4b533862
Azfa <149128367+Azfa34@users.noreply.github.com>
2023-12-24 12:08:59 +0800

    Activity Report

--- app/src/main/java/com/example/ellymartarkedcengal/ActivityReport.java
+++ app/src/main/java/com/example/ellymartarkedcengal/ActivityReport.java
@@ -1,14 +1,39 @@
 package com.example.ellymartarkedcengal;
 
-import androidx.appcompat.app.AppCompatActivity;
+// ActivityReport.java
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityReport extends AppCompatActivity {
+
+    private RecyclerView recyclerViewWishlist;
+    private WishlistAdapter wishlistAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+
+        recyclerViewWishlist = findViewById(R.id.recyclerViewWishlist);
+        recyclerViewWishlist.setLayoutManager(new LinearLayoutManager(this));
+
+
+        wishlistAdapter = new WishlistAdapter(this, getWishlistData());
+        recyclerViewWishlist.setAdapter(wishlistAdapter);
+
+
+    }
+
+
+    private List<Wishlist> getWishlistData() {
+
+        return new ArrayList<>();
     }
 }
